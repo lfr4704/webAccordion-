29ee0a31880bd9298a2a81d729a6dc49b4492931
Luis <lfr4704@gmail.com>
2018-03-12 13:01:48 -0700

    revised

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,18 @@
 $( document ).ready( function () {
+// click is an event listener
+  $("#planets dt").click( function(e){ 
+  	console.log($(this).next().html());
 
-  console.log("Hello world!");
+  	$("#planets dd").css("display", "none");
+  	
+  	//when you do not call for the characteristics of display it assumes it is the default of the property
+  	if( $(this).next().css("display") == "none" ) {
+
+  	$(this).next().css("display", "block");
+  } else {
+  		$(this).next().css("display", "none");
+  }
+  
+  });
 
 });
